04c59fe82c55dcce4b23554557f58f38eac3e258
lzxl30 <2775864744@qq.com>
2026-07-13 20:34:15 +0800

    Update toolsData.js

--- tools/music/toolsData.js
+++ tools/music/toolsData.js
@@ -1,1 +1,12 @@
-
+var musicTools = [
+ {
+  id: 'music-1',
+  title: 'Aidn 创意音乐盒',
+  url: 'https://aidn.jp/contents/',
+  description: '创意互动作品集，包含音调矩阵、初音音乐游戏等，点击即可创作旋律。',
+  category: 'music',
+  icon: '🎵',
+  iconBgClass: 'bg-4',
+  accentClass: 'accent-4'
+}
+];
