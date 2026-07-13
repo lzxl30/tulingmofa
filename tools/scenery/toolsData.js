3f98b89790dc901c53de64acf8cb356803869c74
lzxl30 <2775864744@qq.com>
2026-07-13 13:03:30 +0800

    Update toolsData.js

--- tools/scenery/toolsData.js
+++ tools/scenery/toolsData.js
@@ -1,1 +1,12 @@
-
+var sceneryTools = [
+  {
+    id: 'scenery-1',
+    title: '全景故宫',
+    url: 'https://pano.dpm.org.cn/',
+    description: '故宫博物院官方全景游览平台，在线360°沉浸式参观故宫各大宫殿与庭园。',
+    category: 'scenery',
+    icon: '🏯',
+    iconBgClass: 'bg-5',
+    accentClass: 'accent-5'
+  }
+];
