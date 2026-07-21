3a3caa2cee2c51e93e2267101bea0a2dd4bbf568
lzxl30 <2775864744@qq.com>
2026-07-21 14:15:30 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,3 +1,3 @@
 // 网站版本信息（修改此值即可同步更新缓存和底部显示）
-const siteVersion = "1.9.2";
+const siteVersion = "1.9.3";
 const siteFooterText = `⚡ 趣网站 v${siteVersion} · 内容来自社区分享，建议使用浏览器打开，本站仅提供网站链接导航，不对第三方网站内容负责。访问前请自行甄别。`;
