39d511415ff762a94de1fcdce6dac49aaf63e3eb
lzxl30 <2775864744@qq.com>
2026-07-21 14:47:31 +0800

    Update toolsData.js

--- tools/tools/toolsData.js
+++ tools/tools/toolsData.js
@@ -3,7 +3,7 @@ var toolsTools = [
     id: 'tools-1',
     title: 'Yandex 搜索',
     url: 'https://yandex.com/',
-    description: '俄罗斯最大的搜索引擎，支持全球网页、图片、视频搜索。（个别浏览器不能打开）',
+    description: '俄罗斯最大的搜索引擎，支持全球网页、图片、视频...搜索。（个别浏览器不能打开）',
     category: 'tools',
     icon: '🔍',
     iconBgClass: 'bg-4',      // 蓝紫渐变，理性工具感
